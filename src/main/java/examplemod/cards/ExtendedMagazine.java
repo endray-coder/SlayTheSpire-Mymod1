66f9d71d6e72aa174e180c60a30f094151a18f49
endray <Xty831831@gmail.com>
2026-03-29 23:27:18 +0800

    完成三重射击描述。添加了一张每回合装弹的power

---
+++ src/main/java/examplemod/cards/ExtendedMagazine.java
@@ -0,0 +1,52 @@
+package examplemod.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import examplemod.character.MyCharacter;
+import examplemod.powers.ExtendedMagazinePower;
+
+public class ExtendedMagazine extends CustomCard {
+    public static final String ID = "mymod:ExtendedMagazine";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = cardStrings.NAME;
+    private static final String IMG_PATH = "img/cards/Strike.png";
+    private static final int COST = 2;
+    private static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final CardType TYPE = CardType.POWER;
+    private static final CardColor COLOR = MyCharacter.PlayerColorEnum.EXAMPLE_GREEN;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public ExtendedMagazine() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(1); // 升级后每回合多获得1层子弹
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // 应用ExtendedMagazinePower能力
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
+                p, // 目标
+                p, // 来源
+                new ExtendedMagazinePower(p, this.magicNumber), // 自定义能力
+                this.magicNumber // 层数
+        ));
+    }
+}
---
+++ src/main/java/examplemod/powers/ExtendedMagazinePower.java
@@ -0,0 +1,53 @@
+package examplemod.powers;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class ExtendedMagazinePower extends AbstractPower {
+    public static final String POWER_ID = "mymod:ExtendedMagazinePower";
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private static final String NAME = powerStrings.NAME;
+    private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public ExtendedMagazinePower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+
+        // 添加能力图标
+        String path128 = "img/powers/Example84.png";
+        String path48 = "img/powers/Example32.png";
+        this.region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path128), 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path48), 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        // 每回合开始时获得子弹
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
+                this.owner,
+                this.owner,
+                new bullet(this.owner, this.amount),
+                this.amount
+        ));
+    }
+}
