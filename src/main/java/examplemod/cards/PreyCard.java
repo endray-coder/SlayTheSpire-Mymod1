8aef5cba72a975cea6c620a57c6bc30367fb1039
endray <Xty831831@gmail.com>
2026-03-30 22:57:02 +0800

    添加buff：猎物

---
+++ src/main/java/examplemod/cards/PreyCard.java
@@ -0,0 +1,49 @@
+package examplemod.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import examplemod.character.MyCharacter;
+import examplemod.powers.PreyPower;
+
+public class PreyCard extends CustomCard {
+    public static final String ID = "mymod:PreyCard";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = cardStrings.NAME;
+    private static final String IMG_PATH = "img/cards/Strike.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = MyCharacter.PlayerColorEnum.EXAMPLE_GREEN;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public PreyCard() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBaseCost(0); // 升级后费用-1，变为0费
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // 给予敌人猎物能力
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
+                m,
+                p,
+                new PreyPower(m)
+        ));
+    }
+}
---
+++ src/main/java/examplemod/powers/PreyPower.java
@@ -0,0 +1,68 @@
+package examplemod.powers;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class PreyPower extends AbstractPower {
+    // 能力的ID
+    public static final String POWER_ID = "mymod:PreyPower";
+    // 能力的本地化字段
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    // 能力的名称
+    private static final String NAME = powerStrings.NAME;
+    // 能力的描述
+    private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public PreyPower(AbstractCreature owner) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.type = PowerType.DEBUFF;
+
+        // 不可叠加的能力
+        this.amount = -1;
+
+        // 添加一大一小两张能力图
+        String path128 = "img/powers/Example84.png";
+        String path48 = "img/powers/Example32.png";
+        this.region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path128), 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path48), 0, 0, 32, 32);
+
+        // 首次添加能力更新描述
+        this.updateDescription();
+        PreyPower preyPower = (PreyPower) owner.getPower(POWER_ID);
+        // 施加-5力量
+        if(preyPower==null) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
+                    owner,
+                    owner,
+                    new StrengthPower(owner, -5),
+                    -5
+            ));
+        }
+    }
+
+    // 能力在更新时如何修改描述
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+    // 当能力被移除时，恢复力量
+    @Override
+    public void onRemove() {
+        // 恢复5力量
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
+                owner,
+                owner,
+                new StrengthPower(owner, 5),
+                5
+        ));
+    }
+}
